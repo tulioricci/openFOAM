b58fb3441eb638ff847869c885c5e4f8e628981e
Tulio <tulioricci@gmail.com>
2024-08-22 18:02:20 -0500

    Initial commit with several cases

---
+++ cht/Burner_rotate_axis_uiuc20sp_v1/set_fields.sh
@@ -0,0 +1,5 @@
+cd ./0/fluid/
+rm C2H4 T U
+cd ../../
+cp C2H4 T U ./0/fluid/
+setFields -region fluid
---
+++ cht/weird_Burner_rotate_axis_uiuc20sp_v2/set_fields.sh
@@ -0,0 +1,5 @@
+cd ./0.000000/fluid/
+rm C2H4 T U
+cd ../../
+cp C2H4 T U ./0.000000/fluid/
+setFields -region fluid
