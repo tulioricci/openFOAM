96e61be0b04ccc6b319ee526a4d88b45b3943d2f
Tulio <tulioricci@gmail.com>
2024-08-27 18:42:59 -0500

    Reorganize files (rm)

--- cht/Burner_rotate_axis_uiuc20sp_v1/set_fields.sh
+++
@@ -1,5 +0,0 @@
-cd ./0/fluid/
-rm C2H4 T U
-cd ../../
-cp C2H4 T U ./0/fluid/
-setFields -region fluid
--- cht/weird_Burner_rotate_axis_uiuc20sp_v2/set_fields.sh
+++
@@ -1,5 +0,0 @@
-cd ./0.000000/fluid/
-rm C2H4 T U
-cd ../../
-cp C2H4 T U ./0.000000/fluid/
-setFields -region fluid
