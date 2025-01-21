6713290690c7eefeac595f95af73027a6c95d5c4
Tulio <tulioricci@gmail.com>
2025-01-21 09:18:11 -0600

    Update reactingDNS -- seems to be working great now

--- OpenFOAM-addons/applications/solvers/combustion/reactingDNS/reactingDNS.C
+++ OpenFOAM-addons/applications/solvers/combustion/reactingDNS/reactingDNS.C
@@ -142,7 +142,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        rho = thermo.rho();
+        rho = thermo.rho();  
 
         runTime.write();
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
