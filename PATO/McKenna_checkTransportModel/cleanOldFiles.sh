10d9c6aff38677a797cd91d54b287e347806de0a
Tulio <tulioricci@gmail.com>
2025-05-29 09:59:57 -0500

    Small tweaks to the burner reactingDNS case

--- PATO/McKenna_checkTransportModel/cleanOldFiles.sh
+++
@@ -1,6 +0,0 @@
-for i in {0..5}
-do
-        for file in $(ls -dt processor${i}/[0-9]*/ | tail -n+6); do mkdir -p __trash__/processor${i}/; mv -v $file __trash__/processor${i}; done
-done
-rm -rf __trash__
-
